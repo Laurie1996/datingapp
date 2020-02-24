a401d3105bb8a52f6eaa431447a49a9b3953008e
Laurie1996 <58736640+Laurie1996@users.noreply.github.com>
2020-02-24 13:05:33 +0100

    relocated

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+
+const express= require('express');
+const app = express();
+const port = 8000;
+
+app.use('/static',express.static('public'));
+
+app.get('/',(req, res)=> {
+  res.write('<p>Hello World!<p>');
+  res.end();
+});
+
+app.listen(port, () => console.log('Running my NOdeJS server at'));
